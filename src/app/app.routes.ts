a13ed0cb1d9529c4f78c6dd9f34c66e70197da02
ayoub-oumha <ayyouboumha@gmail.com>
2026-02-01 22:47:30 +0100

    feat: add auth guard to protect dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Logi } from './pages/logi/logi';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Logi },
-  { path: 'dashboard', component: Dashboard },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
 ];
